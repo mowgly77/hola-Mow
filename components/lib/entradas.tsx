94ec9080aef9d707aab05956e3295f74c7293921
RobertoEAlcantara <robertoe.alcantara@gmail.com>
2025-03-31 19:14:49 -0600

    api url

--- components/lib/entradas.tsx
+++ components/lib/entradas.tsx
@@ -4,6 +4,6 @@ import axios from 'axios';
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const getEntradas = async () => {
-  const res = await axios.get(`${API_URL}api/entradas?populate=*`);
+  const res = await axios.get(`${API_URL}/entradas?populate=*`);
   return res.data.data;
 };
